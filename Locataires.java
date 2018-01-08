8c6bedb5d79046e5bd270d6fb8d77b255c4d3b40
FredPhg <34663048+FredPhg@users.noreply.github.com>
2018-01-08 20:22:51 +0100

    Add files via upload

--- Locataires.java
+++ Locataires.java
@@ -1,48 +1,38 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
-class Locataires {
-	Scanner saisie = new Scanner ( System.in ) ;
+public class Locataires {
+	Scanner saisie = new Scanner ( System.in );
 
-	int[] creerVLocId() { // création d'un tableau de 500 cases ( identifiants adhérent)
-		int vLocId[] = new int [500] ;
-	/*	for ( int i = 0; i < 500 ; i++) {
-			vLocId[i] = 0 ;
-		}
-	*/	return vLocId;
-	}
-	int[] creerVLocNo() { // création d'un tableau de 500 cases ( noms d'adhérents)
-		int vLocNo[] = new int [500] ;
-		return vLocNo;
-	}
-	String[] creerVLocAd() { // création d'un tableau de 500 cases (adresses d'adhérents)
-		String vLocAd[] = new String [500] ;
-		return vLocAd;
-	}
-/*	int[][] creerVLocBi() { // création de 500 tab de 5 id de biens à louer ou louer
-		int tab[][] = new int [500][5] ;
-		return tab[];
-	}*/
+	
+	int vLocId[] = new int [3]; // création d'un tableau de 500 cases ( identifiants adhérent)
+	String vLocNo[] = new String [500]; // création d'un tableau de 500 cases ( noms d'adhérents)
+	String vLocAd[] = new String [500]; // création d'un tableau de 500 cases (adresses d'adhérents)
+//	int vLocBi[] = new int [5]; // création de 500 tab de 5 id de biens à louer ou loué
 
-	void afficherVLocId(int[] vLocId) {
+	int idLoc;
+	String nomLoc;
+	String adresLoc;
+
+	public void afficher() {
 		System.out.print("ID");
-		System.out.println() ;
-		String affichage = " " ;
+		System.out.println();
+		String affichage = " ";
 		for ( int i = 0 ; i < vLocId.length ; i++) {
 			if ( vLocId[i] != 0 ) {
-			affichage = affichage + vLocId[i] ;
-			System.out.println(affichage) ;
+			affichage = affichage + vLocId[i];
+			System.out.println(affichage);
 			}
+		//	}else{ System.out.println("Le tableau est vide");
 		}
-
 	}
     
-	void ajoutModSupLoc() {
+	public void ajoutModSupLoc() {
 		System.out.println("~ a ~ Ajouter un locataire");
 		System.out.println("~ b ~ Modifier un locataire");
 		System.out.println("~ c ~ Supprimer un locataire");
 		System.out.println("~ d ~ Retour au menu précédent");
-		System.out.println("");
+		System.out.println();
 		System.out.println("Saisir votre choix entre a et d");
 		System.out.println("");
 		switch ( saisie.nextInt()){
@@ -61,23 +51,40 @@ class Locataires {
 		}
 	}
 	
- 	void ajoutLoc(vLocNo[]) { // ajouter un Locataire
- 		int i = 0;
-    	System.out.print("Saisir un numero d'adhérent : ");
-    	Scanner id = new Scanner ( System.in ) ;
-  		vLocNo[i] = id;
-  		i++;		
+    public void ajoutLoc() { // ajouter un Locataire
+ 		System.out.println("Saisir un numérod'adhérent : ");
+ 		idLoc = saisie.nextInt();
+
+ 		System.out.println("Saisir un nom : ");
+ 		nomLoc = saisie.next();
+
+ 		System.out.println("Saisir une adresse : ");
+ 		adresLoc = saisie.next();
+
+ 		for ( int i = 0 ; i < vLocId.length ; i++ ) {
+ 			if ( vLocId[i] == 0 ); // vérifier la case vide
+ 			vLocId[i] = idLoc;
+ 			vLocNo[i] = nomLoc; 
+ 			vLocAd[i] = adresLoc;
+
+
+
+ 		}
+
+  			
 	}
 	  
 
-
 /*	void modifierLoc() {
+
+
 	}*/
 
 /*	void supprimerLoc() {
 		System.out.println("Supprimer quel ID ?");
     	Scanner id = new Scanner ( System.in ) ;
   		vLocId.remove(id.nextInt());
+
 	}*/
 
 }
@@ -85,19 +92,22 @@ class Locataires {
 class LocatairesTest {
     public static void main(String args[]) {
 
- //   ArrayList<Integer> tab = new Arraylist<Integer>() ;
-
     Locataires g = new Locataires ();
-    int []tab = g.creerVLocId();
+//   int vLocId[] = new int [500]; // création d'un tableau de 500 cases ( identifiants adhérent)
+//  int []tab = g.creerVLocId();
 
 //  g.ajoutLoc();
-    g.creerVLocId();
+//  g.creerVLocId();
 //  g.creerVLocNo();
 //  g.creerVLocAd();
 
-    System.out.print("_____________TABLEAU______________");
-    System.out.println("");
-    g.afficherVLocId(tab);
+    
+
+    g.ajoutLoc();
+    System.out.println();
+    System.out.println("_____________TABLEAU______________");
+    System.out.println();
+    g.afficher();	
 
 
     }
